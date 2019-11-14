bda9d68883cd7cf0739cab8fd3366b1aa53761c2
lusensama <lusensama@gmail.com>
2019-11-13 19:58:30 -0500

    adjust learning rate

--- ImageNet/networks/main.py
+++ ImageNet/networks/main.py
@@ -565,7 +565,7 @@ class AverageMeter(object):
 
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.5 ** (epoch // 4))
+    lr = args.lr * (0.5 ** (epoch // 5))
     print ('Learning rate:', lr)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
