46a574b0875a7d6bb7529caadb41b72d4ddf3451
lusensama <lusensama@gmail.com>
2019-11-28 21:45:55 -0500

    added vgg15_bn_xnor

--- CIFAR_100/main.py
+++ CIFAR_100/main.py
@@ -188,10 +188,10 @@ if __name__ == '__main__':
 
         model.features = torch.nn.DataParallel(model.features)
         # model = torch.nn.DataParallel(model)
-        model.cuda()
+        model.cuda(0)
         model.load_state_dict(pretrained_model['state_dict'])
 
-        model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+        model = torch.nn.DataParallel(model, device_ids=0)
 
 
 
